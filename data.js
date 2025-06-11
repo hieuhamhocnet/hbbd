5064a862b672867bcc1a729ca8401aef0b5692ec
Nguyen Minh Hieu <minhhieuit.info@gmail.com>
2025-06-11 18:20:34 +0700

    Deploy Web SN fix lỗi

---
+++ data.js
@@ -0,0 +1,20 @@
+// data.js
+const galleryImages = [
+  { src: "images/love1.jpg" },
+  { src: "images/love2.jpg" },
+  { src: "images/love3.jpg" },
+  { src: "images/love4.jpg" },
+  { src: "images/love5.jpg" },
+  { src: "images/love6.jpg" },
+  { src: "images/love7.jpg" }
+];
+
+const loveReasons = [
+  "Vì nụ cười của em tỏa nắng hơn cả mặt trời.",
+  "Vì em luôn biết cách làm anh cười ngay cả khi buồn nhất.",
+  "Vì ở bên em, anh thấy bình yên đến lạ.",
+  "Vì em là cô gái thông minh và mạnh mẽ nhất anh từng biết.",
+  "Vì em nấu ăn rất ngon",
+  "Vì mỗi ngày bên em đều là một cuộc phiêu lưu thú vị.",
+  "Đơn giản... vì em là em. 💓"
+];
